606483a051f1e0e18a339e8308e5ce6a77b46650
tusharpamnani <tusharpamnani1@gmail.com>
2024-06-10 03:35:26 +0530

    irate-calculator

---
+++ config-example.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = 'https://your-supabase-url.supabase.co';
+export const SUPABASE_ANON_KEY = 'your-supabase-anon-key';
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,13 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../../config';
 
-const supabaseUrl = "https://efcbhjrflueteanacnyt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmY2JoanJmbHVldGVhbmFjbnl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5MzIzMjEsImV4cCI6MjAzMzUwODMyMX0.5yppSt6sWwndMtu2-QYOFN1KAxUv2Zwng5KM7fAd3W0";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+// const supabaseUrl = process.env.SUPABASE_URL as string;
+// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
