a1b4676b3027319d64d7611319d70a41325c7005
tusharpamnani <tusharpamnani1@gmail.com>
2024-06-09 18:45:19 +0530

    ludicrous-spot

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
-import { supabase } from "./src/lib/supabase";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform, StatusBar } from "react-native";
+import { supabase } from "../lib/supabase";
 import { useEffect, useState } from "react";
 
 // internal import
-import MovieItem from "./src/components/MovieItem";
+import MovieItem from "../components/MovieItem";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchMovies = async () => {
-      let { data: movies, error } = await supabase
+      const { data: movies } = await supabase
         .from("movies")
         .select("*")
         .range(0, 24);
-
-      // console.log(error)
-      // console.log(JSON.stringify(movies, null, 2))
-
-      if (movies) {
-        setMovies(movies);
-      }
+      setMovies(movies);
     };
     fetchMovies();
   }, []);
 
   return (
     <View style={styles.container}>
-      <SafeAreaView>
+      <SafeAreaView style={styles.AndroidSafeArea}>
         <FlatList data={movies} renderItem={MovieItem} />
       </SafeAreaView>
       <StatusBar style="auto" />
@@ -40,6 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#181413",
-    paddingTop: 50,
   },
+  AndroidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,4 +1,5 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type MovieItemPros = {
   item: any;
@@ -7,13 +8,15 @@ type MovieItemPros = {
 
 const MovieItem = ({ item: movie, index }: MovieItemPros) => {
   return (
-    <Pressable style={styles.container}>
-      <Text style={styles.index}>#{index + 1}</Text>
-      <View style={{ flex: 1 }}>
-        <Text style={styles.title}>{movie.title}</Text>
-        <Text style={styles.subtitle}>{movie.tagline}</Text>
-      </View>
-    </Pressable>
+    <Link href={`/${movie.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Text style={styles.index}>#{index + 1}</Text>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.subtitle}>{movie.tagline}</Text>
+        </View>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -21,23 +24,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   index: {
     fontSize: 40,
-    color: 'gray',
-    fontFamily: 'Courier New',
+    color: "gray",
+    fontFamily: "lucida grande",
     marginRight: 15,
   },
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: 'gainsboro',
+    fontWeight: "bold",
+    color: "gainsboro",
     marginVertical: 5,
   },
   subtitle: {
-    color: 'gray',
+    color: "gray",
   },
 });
 
